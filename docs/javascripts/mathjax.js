58bd60ddb8fd425bdde4c6e0c8ae67e18117af91
gatocor <g.torregrosa@outlook.com>
2026-02-06 14:11:34 +0100

    render examples appropiately

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,16 +1,12 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"], ["$", "$"]],
-    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
     processEscapes: true,
     processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*",
-    processHtmlClass: "arithmatex|jp-RenderedHTMLCommon|md-content"
   }
 };
 
 document$.subscribe(() => {
-  MathJax.typesetPromise()
-})
+  MathJax.typesetPromise();
+});
